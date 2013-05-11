8deb98a7b96927abff9855b351b0a7abc1610566
ErinaMikami <erina.m29@gmail.com>
2013-05-12 04:51:34 +0900

    userのルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Oreshio::Application.routes.draw do
   get '/auth/:provider/callback', to: 'users#auth_callback'
 
   resources :stories
-  resources :users do
+  resources :users, except: [ :index, :show ] do
     collection do
       get :login
       get :logout
