e3ab1b67477464a7337ee180d5021627fc0123ef
ErinaMikami <erina.m29@gmail.com>
2013-05-12 04:39:02 +0900

    そんな重要なファイルがgitignoreされてるなんて人生棒に振った気分ですわ

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_COMSUMER_SECRET']
-  provider :facebook, '129745623888871', 'bc7470c3bb0a1dbdbe8d46e87fce3455', :scope => 'publish_stream'
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], :scope => 'publish_stream'
 end
