4b5f9bfd4f135e22742956e4b4f8189c654c6f56
ErinaMikami <erina.m29@gmail.com>
2013-05-12 03:59:15 +0900

    SNS Key指定のとこ修正

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'publish_stream'
+  provider :twitter, ['TWITTER_KEY'], ['TWITTER_SECRET']
+  provider :facebook, ['FACEBOOK_KEY'], ['FACEBOOK_SECRET'], :scope => 'publish_stream'
 end
