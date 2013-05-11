3dd426618570ea3be0921380637d70108081fba6
ErinaMikami <erina.m29@gmail.com>
2013-05-12 04:33:38 +0900

    何回設定すればいいんだ！

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_COMSUMER_SECRET']
-  provider :facebook, '139126549603654', '8fccd19bb9dcbe28a2d442fe535a12fe', :scope => 'publish_stream'
+  provider :facebook, '129745623888871', 'bc7470c3bb0a1dbdbe8d46e87fce3455', :scope => 'publish_stream'
 end
