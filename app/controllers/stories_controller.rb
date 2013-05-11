27796e5962c1d9c2c5df73884c810f6d8c9715d6
ErinaMikami <erina.m29@gmail.com>
2013-05-12 03:18:52 +0900

    SNSログイン機能を実装
    下記、未対応
    ・ログインしてるユーザー情報の表示
    ・ユーザーの編集機能
    ・後からもう片方のSNSへ紐付ける

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,83 +1,8 @@
-class StoriesController < ApplicationController
-  # GET /stories
-  # GET /stories.json
-  def index
-    @stories = Story.all
+class StoriesController < InheritedResources::Base
+  before_filter :login_check
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @stories }
-    end
+  def login_check
+    redirect_to login_users_path unless session[:login_user]
   end
 
-  # GET /stories/1
-  # GET /stories/1.json
-  def show
-    @story = Story.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @story }
-    end
-  end
-
-  # GET /stories/new
-  # GET /stories/new.json
-  def new
-    @story = Story.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @story }
-    end
-  end
-
-  # GET /stories/1/edit
-  def edit
-    @story = Story.find(params[:id])
-  end
-
-  # POST /stories
-  # POST /stories.json
-  def create
-    @story = Story.new(params[:story])
-
-    respond_to do |format|
-      if @story.save
-        format.html { redirect_to @story, notice: 'Story was successfully created.' }
-        format.json { render json: @story, status: :created, location: @story }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @story.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /stories/1
-  # PUT /stories/1.json
-  def update
-    @story = Story.find(params[:id])
-
-    respond_to do |format|
-      if @story.update_attributes(params[:story])
-        format.html { redirect_to @story, notice: 'Story was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @story.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /stories/1
-  # DELETE /stories/1.json
-  def destroy
-    @story = Story.find(params[:id])
-    @story.destroy
-
-    respond_to do |format|
-      format.html { redirect_to stories_url }
-      format.json { head :no_content }
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Oreshio::Application.routes.draw do
+  get '/auth/:provider/callback', to: 'users#auth_callback'
+
   resources :stories
-  resources :users
+  resources :users do
+    collection do
+      get :login
+      get :logout
+    end
+  end
+
   root :to => 'statics#home'
 end
