6e661ddc67781870c19f7a607e6569ecc85f7b6d
ErinaMikami <erina.m29@gmail.com>
2013-05-12 09:01:13 +0900

    とりあえず、しおりが作成できる
    アソシエーション周り、全然いれてないよ

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,16 +1,28 @@
 class Story < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :sub_title, :event_day, :memo, :rule
 
-  #ユーザ=>参加者
-  has_many :users, :through => :story_users
+  # ========= association =========
 
   #ユーザ
   belongs_to :user
 
+  #ユーザ=>参加者
+  has_many :users, :through => :story_users
+
   #めぐリスト
   has_many :visiteds
+
   #持ち物
   has_many :items
+
   #スケジュール
   has_many :schedules
+
+  # ========= validation =========
+
+  validates :title, presence: true
+  validates :rule, presence: true
+  validates :event_day, presence: true
+
+
 end
